6462ecf2b2d4f49a143e9215ed50c5a17576b019
Jyri Lutsius <jyril81@gmail.com>
2015-06-13 17:45:55 +0300

    hanoi towers another slower recursive solution

--- hanoi/src/main/java/com/jyril/Hanoi.java
+++ hanoi/src/main/java/com/jyril/Hanoi.java
@@ -6,19 +6,60 @@ package com.jyril;
 public class Hanoi {
 
     public static void main(String[] args) {
-        int nrMovesDone = myhanoi(3, 1, 3, 2, 0);
+        int nrMovesDone = playHanoy(3, 1, 3, 2, 0);
         System.out.println("Nr moves done " + nrMovesDone);
     }
 
-    public static int myhanoi(int nrdisksToMove, int from, int to, int spare, int nrMovesDone) {
+    /**
+     * Classical recursive solution that solves with min nr of moves
+     *
+     * @param nrdisksToMove
+     * @param from
+     * @param to
+     * @param spare
+     * @param nrMovesDone
+     * @return
+     */
+    public static int classicHanoi(int nrdisksToMove, int from, int to, int spare, int nrMovesDone) {
         if (nrdisksToMove >= 1) {
             //move n-1 disks from->spare
-            nrMovesDone = myhanoi(nrdisksToMove - 1, from, spare, to, nrMovesDone);
+            nrMovesDone = classicHanoi(nrdisksToMove - 1, from, spare, to, nrMovesDone);
             //move 1 disk from->to
             System.out.println("Moving from " + from + "to " + to);
             nrMovesDone++;
             //move n-1 disks spare->to
-            nrMovesDone = myhanoi(nrdisksToMove - 1, spare, to, from, nrMovesDone);
+            nrMovesDone = classicHanoi(nrdisksToMove - 1, spare, to, from, nrMovesDone);
+            return nrMovesDone;
+        }
+        return nrMovesDone;
+    }
+
+
+    /**
+     * Another recursive solution I tried to play with recursion
+     * It works (finally resulting in desired state)but does not give min nr of moves ...
+     *
+     * @param nrdisksToMove
+     * @param from
+     * @param to
+     * @param spare
+     * @param nrMovesDone
+     * @return
+     */
+    public static int playHanoy(int nrdisksToMove, int from, int to, int spare, int nrMovesDone) {
+        if (nrdisksToMove >= 1) {
+            //move n-1 disks from-->to
+            nrMovesDone = playHanoy(nrdisksToMove - 1, from, to, spare, nrMovesDone);
+            //move 1 disk from from-->spare
+            System.out.println("Moving from " + from + "to " + spare);
+            nrMovesDone++;
+            //move n-1 disks to-->from
+            nrMovesDone = playHanoy(nrdisksToMove - 1, to, from, spare, nrMovesDone);
+            //move 1 disk spare--->to
+            System.out.println("Moving from " + spare + "to " + to);
+            nrMovesDone++;
+            //move n-1 disks from-->to
+            nrMovesDone = playHanoy(nrdisksToMove - 1, from, to, spare, nrMovesDone);
             return nrMovesDone;
         }
         return nrMovesDone;
--- hanoi/src/test/java/com/jyril/HanoiTest.java
+++ hanoi/src/test/java/com/jyril/HanoiTest.java
@@ -11,28 +11,28 @@ public class HanoiTest {
 
     @Test
     public void noDisksIsNoMoves() throws Exception {
-        assertEquals(0, Hanoi.myhanoi(0, 1, 3, 2, 0));
+        assertEquals(0, Hanoi.classicHanoi(0, 1, 3, 2, 0));
     }
 
     @Test
     public void onwDiskIsOneMove() throws Exception {
-        assertEquals(1, Hanoi.myhanoi(1, 1, 3, 2, 0));
+        assertEquals(1, Hanoi.classicHanoi(1, 1, 3, 2, 0));
     }
 
     @Test
     public void twoDisksIs3Moves() throws Exception {
-        assertEquals(3, Hanoi.myhanoi(2, 1, 3, 2, 0));
+        assertEquals(3, Hanoi.classicHanoi(2, 1, 3, 2, 0));
     }
 
     @Test
     public void threeDisksIs7Moves() throws Exception {
-        assertEquals(7, Hanoi.myhanoi(3, 1, 3, 2, 0));
+        assertEquals(7, Hanoi.classicHanoi(3, 1, 3, 2, 0));
     }
 
     @Test
     public void nDisksIs2PowernMinus1() throws Exception {
         int nrDisks = 10;
-        assertEquals((int) Math.pow(2, 10) - 1, Hanoi.myhanoi(10, 1, 3, 2, 0));
+        assertEquals((int) Math.pow(2, 10) - 1, Hanoi.classicHanoi(10, 1, 3, 2, 0));
     }
 
 }
